912cab05b5901c888103111b02415842393ce294
johair84 <33111357+johair84@users.noreply.github.com>
2020-08-10 17:54:47 +0600

    ture vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy values
+// 0
+// ""
+// undefined (not declared)
+// null
+// NaN = Not a Number
+// false
+
+//truthy values
+//'0', ' ' , []
+
+
+let name = 0;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
