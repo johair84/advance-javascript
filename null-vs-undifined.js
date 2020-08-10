639709daf39e6b1bcc3012007b7bff34450b7ad1
johair84 <33111357+johair84@users.noreply.github.com>
2020-08-10 18:11:23 +0600

    done

---
+++ null-vs-undifined.js
@@ -0,0 +1,23 @@
+let name;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+    //if funtion not return anything
+
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+
+const premik = {name: "smart dude", phone: 2323333};
+console.log(premik.gf);
+
+never do it
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,34,5,2];
+console.log(ages[21]);
